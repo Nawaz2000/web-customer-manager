45ebc138995b2c232685a8e3aee34abf18aa4037
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-06-09 15:39:22 +0530

    search further optimized

--- src/main/java/com/nawaz2000/spring/dao/CustomerDAOimpl.java
+++ src/main/java/com/nawaz2000/spring/dao/CustomerDAOimpl.java
@@ -14,21 +14,20 @@ import com.nawaz2000.spring.entity.Customer;
 
 @Repository("customerDAOimpl")
 public class CustomerDAOimpl {
-	
+
 	@Autowired
 	@Qualifier("sessionFactory")
 	SessionFactory sessionFactory;
-	
-	public void addCustomer(Customer customer) {	
+
+	public void addCustomer(Customer customer) {
 		Session session = sessionFactory.getCurrentSession();
 		session.saveOrUpdate(customer);
-		
+
 	}
 
 	public List<Customer> listCustomers() {
 		Session session = sessionFactory.getCurrentSession();
-		List<Customer>customerList = session.createQuery("from Customer", 
-				Customer.class).getResultList();
+		List<Customer> customerList = session.createQuery("from Customer", Customer.class).getResultList();
 		return customerList;
 	}
 
@@ -41,32 +40,21 @@ public class CustomerDAOimpl {
 		Session session = sessionFactory.getCurrentSession();
 		Customer retrievedCustomer = session.get(Customer.class, id);
 		session.delete(retrievedCustomer);
-		
+
 	}
 
 	public List<Customer> searchCustomers(String searchName) {
 		Session session = sessionFactory.getCurrentSession();
-		if (searchName != null && searchName.trim().length()>0) {
-			String[] name = searchName.split("\\s+");
-			if (name.length == 2)
-			{
-				String fName = name[0];
-				String lName = name[1];
-				Query query = session.createQuery("from Customer where lower(firstName) = " + "'" + fName.toLowerCase() + "'" + " AND lower(lastName) = " + "'" + lName.toLowerCase() + "'", Customer.class);
-				System.out.println("My query: " + query);
-				return query.getResultList();
-			}else if(name.length == 1) {
-				String fName = name[0];
-				Query query = session.createQuery("from Customer where lower(firstName) = " + "'" + fName.toLowerCase() + "'", Customer.class);
-				System.out.println("My query: " + query);
-				return query.getResultList();
-			}else
-				return new ArrayList<Customer>();
-			
-		}else {
+		if (searchName != null && searchName.trim().length() > 0) {
+			Query query = session.createQuery("from Customer where lower(firstName) LIKE " + "'%" + searchName.toLowerCase()
+					+ "%'" + " OR lower(lastName) LIKE " + "'%" + searchName.toLowerCase() + "%'", Customer.class);
+			System.out.println("My query: " + query);
+			return query.getResultList();
+
+		} else {
 			List<Customer> searchResult = new ArrayList<Customer>();
 			return searchResult;
 		}
 	}
-	
+
 }
